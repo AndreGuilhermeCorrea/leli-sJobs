b561a1482577143b3a0ba1adef619f820d3950fb
AndreGuilhermeCorrea <de_correa@msn.com>
2023-11-17 16:11:12 -0300

    Atualização da Main

--- main.py
+++ main.py
@@ -13,12 +13,44 @@ class LelisApp(MDApp, App):
     KV_FILES = {
         os.path.join(os.getcwd(), "view/screenmanager.kv"),
         os.path.join(os.getcwd(), "view/loginScreen.kv"),
-     
+        os.path.join(os.getcwd(), "view/servicosScreen.kv"),
+        os.path.join(os.getcwd(), "view/alvenariaScreen.kv"),
+        os.path.join(os.getcwd(), "view/hidraulicaScreen.kv"),
+        os.path.join(os.getcwd(), "view/eletricaScreen.kv"),
+        os.path.join(os.getcwd(), "view/cuidadosScreen.kv"),
+        os.path.join(os.getcwd(), "view/petScreen.kv"),
+        os.path.join(os.getcwd(), "view/techScreen.kv"),
+        os.path.join(os.getcwd(), "view/limpezaScreen.kv"),
+        os.path.join(os.getcwd(), "view/jardinagemScreen.kv"),
+        os.path.join(os.getcwd(), "view/outrosScreen.kv"),
+        os.path.join(os.getcwd(), "view/cadastrarScreen.kv"),
+        os.path.join(os.getcwd(), "view/teladoisScreen.kv"),
+        os.path.join(os.getcwd(), "view/editarperfilScreen.kv"),
+        os.path.join(os.getcwd(), "view/favoritosScreen.kv"),
+        os.path.join(os.getcwd(), "view/listartodosScreen.kv"),      
+  
     }
 
     #classes que controlam os kv's
     CLASSES = {
         "MainScreenManager": "controller.screenmanager",
+        "LoginScreen": "controller.loginScreen",
+        "ServicosScreen": "controller.servicosScreen",
+        "AlvenariaScreen": "controller.alvenariaScreen",
+        "HidraulicaScreen": "controller.hidraulicaScreen",
+        "EletricaScreen": "controller.eletricaScreen",
+        "CuidadosScreen": "controller.cuidadosScreen",
+        "PetScreen": "controller.petScreen",
+        "TechScreen": "controller.techScreen",
+        "LimpezaScreen": "controller.limpezaScreen",
+        "JardinagemScreen": "controller.jardinagemScreen",
+        "OutrosScreen": "controller.outrosScreen",
+        "CadastrarScreen": "controller.cadastrarScreen",
+        "TelaDoisScreen": "controller.teladoisScreen",
+        "EditarPerfilScreen": "controller.editarperfilScreen",
+        "FavoritosScreen": "controller.favoritosScreen",
+        "ListarTodosScreen": "controller.listartodosScreen",        
+   
         
     }
 
