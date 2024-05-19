8a2af7ea94a0910ef5801c6ded8ecdde573c41f5
AndreGuilhermeCorrea <105138998+AndreGuilhermeCorrea@users.noreply.github.com>
2024-05-19 10:35:24 -0300

    Update data.py
    Changes data

--- model/data.py
+++ model/data.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from model.emailUser import EmailUser
 
 class data:
 
@@ -9,17 +10,21 @@ class data:
     firebase = 'https://lelisbase-default-rtdb.firebaseio.com'
     
     usuario = f'{firebase}/usuarios/{cpf}'
-   
+
+    # Chama a função para recuperar o email do usuário
+    sender_email = requests.get(f'{usuario}/email.json')
+
     #get (consulta)
     consulta = requests.get(f'{usuario}/senha.json',data=json.dumps(senha))
-    resposta = consulta.text   
+
+    resposta = consulta.text  
     senha2 = '"'+senha+'"'
+    
+    EmailUser.set_email(sender_email.text)
+    remetente = EmailUser.get_email()
+    print(remetente)
     
     if(senha2 ==  resposta):
       return True
     else:
       return False
-
-
-
-
