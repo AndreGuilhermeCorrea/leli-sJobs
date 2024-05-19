cb37f5fb06c2a4b2bc78345971d692062b0fb39d
AndreGuilhermeCorrea <105138998+AndreGuilhermeCorrea@users.noreply.github.com>
2024-05-19 10:27:01 -0300

    Update eletricaScreen.py
    Changes

--- controller/eletricaScreen.py
+++ controller/eletricaScreen.py
@@ -1,36 +1,9 @@
 from kivymd.uix.screen import MDScreen
-
+import sys
 
 class EletricaScreen(MDScreen):
     pass
 
-    def tela_alvenaria(self):
-        self.manager.current = "alvenariaScreen"
-
-    def tela_hidraulica(self):
-        self.manager.current = "hidraulicaScreen"
-
-    def tela_eletrica(self):
-        self.manager.current = "eletricaScreen"
-
-    def tela_cuidados(self):
-        self.manager.current = "cuidadosScreen"
-
-    def tela_pet(self):
-        self.manager.current = "petScreen"
-
-    def tela_tech(self):
-        self.manager.current = "techScreen"
-        
-    def tela_limpeza(self):
-        self.manager.current = "limpezaScreen"
-
-    def tela_jardinagem(self):
-        self.manager.current = "jardinagemScreen"
-
-    def tela_outros(self):
-        self.manager.current = "outrosScreen"
-        
     def voltar_para_home(self):
         self.manager.current = "teladoisScreen"
         
@@ -42,3 +15,12 @@ class EletricaScreen(MDScreen):
         
     def listar(self):
         self.manager.current = "listartodosScreen"
+    
+    def tela_cadastrados(self): 
+        self.manager.current = "perfisScreen"
+
+    def volta(self):
+        self.manager.current = "servicosScreen"
+
+    def sair(self):
+        sys.exit()         
