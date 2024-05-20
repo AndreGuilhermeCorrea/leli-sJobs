beb113663ccce3a5b4e6ba6592ca0b8252d9f560
ERIC LINCOLN DEMATE <64226050+EricDemate@users.noreply.github.com>
2024-05-19 23:39:25 -0300

    Update loginScreen.py
    Validação de Campos: O método teste_campos verifica se os campos cpf e senha estão preenchidos corretamente e exibe mensagens de erro apropriadas se não estiverem. Ele também retorna False se algum campo estiver incorreto e True se todos os campos estiverem corretos.
    Limpeza de Campos: Os métodos limpar_erro_label e limpar_dados são usados para limpar os campos de entrada de texto, com o primeiro também limpando a mensagem de erro.
    Sair do Aplicativo: O método sair encerra o aplicativo.
    Navegação para Tela de Cadastro: O método cadastrar altera a tela atual para a tela de cadastro.
    Esses métodos juntos garantem que a tela de login funcione corretamente, validando as entradas do usuário, exibindo mensagens de erro apropriadas e permitindo a navegação para outras partes do aplicativo.

--- controller/loginScreen.py
+++ controller/loginScreen.py
@@ -31,3 +31,44 @@ class LoginScreen(MDScreen):
         else:   
             error_label.text = "Dados nao conferem"  
             Clock.schedule_once(lambda dt: self.limpar_erro_label(), 2)
+
+def teste_campos(self, cpf, senha):
+
+        error_label = self.ids.error_label       
+        
+        if(cpf == '')and( senha == ''):
+            error_label.text = "Insira os dados para logar"
+            Clock.schedule_once(lambda dt: self.limpar_erro_label(), 2)
+            return False
+        elif(senha == ''):
+            error_label.text = "Insira a senha para logar"
+            Clock.schedule_once(lambda dt: self.limpar_erro_label(), 2)
+            return False
+        elif(cpf == '' ):           
+            error_label.text = "Insira o CPF para logar"  
+            Clock.schedule_once(lambda dt: self.limpar_erro_label(), 2)
+            return False
+        elif(len(cpf) != 11 ):           
+            error_label.text = "Dados incorretos"  
+            Clock.schedule_once(lambda dt: self.limpar_erro_label(), 2)
+            return False
+        else:
+            return True
+            
+    def limpar_erro_label(self):
+        # Limpe o texto da label
+        self.ids.error_label.text = ""
+        self.ids.cpf.text = ""  
+        self.ids.password.text = ""
+        
+    def limpar_dados(self):
+        self.ids.cpf.text = ""  
+        self.ids.password.text = ""
+        
+    def sair(self):
+        sys.exit()
+        
+    def cadastrar(self):
+        self.manager.current = "cadastrarScreen"
+
+
